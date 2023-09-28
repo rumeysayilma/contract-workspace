86a2087b83e6f1dadb21f7d6ca8034aadf77ac6d
 rumeysayilma <yilmazr16@itu.edu.tr>
2023-09-29 00:04:26 +0300

    mapping

--- contracts/SimpleContract.sol
+++ contracts/SimpleContract.sol
@@ -6,26 +6,36 @@ contract SimpleContract {
     bool public allowed;
     uint public count;
     int public signedCount;
-    string private errorMessage = "is not Allowed";
-    mapping(address => bool) public allowance; //mapping icinde erisilmek istenen adres buraya gonderilir. karsılıgında bool 1/0 dönülür
+    //string private errorMessage = "is not Allowed";
+    //mapping(address => bool) public allowance; //mapping icinde erisilmek istenen adres buraya gonderilir. karsılıgında bool 1/0 dönülür
     
+    mapping(address => mapping(address => bool) ) public allowance;
+
+    //error mesaj listesi olusturalım:
+    string[2] public errorMessages;  //fixed size array
+    //string[] public errorMessages;  //unfixed size
     address public owner; //hangi adres bu contract ın sahibi olacak
 
     //constructor tanımlamamız lazım:
-    constructor(){
-        owner=address(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+    constructor(address _address){
+        owner=_address;
+        errorMessages[0] = "is not allowed";
+        errorMessages[1] = "only owner";
+        //errorMessages.push("is not allowed"); 
+        //errorMessages.push("only owner"); 
+
     }
 
-    function setAllowed(bool _allowed) public {
-        allowed = _allowed;
+    function setAllowed(bool _owner) public {
+        allowed = _owner;
     }
     modifier isAllowed() {
-        require(allowance[msg.sender],errorMessage);
+        require(allowance[owner][msg.sender],errorMessages[0]);
         _;
     }
 
     modifier OnlyOwner(){
-        require(owner == msg.sender, "only owner" );
+        require(owner == msg.sender, errorMessages[1] );
         _;
     }
 
@@ -46,9 +56,12 @@ contract SimpleContract {
     }
 
     function assignAllowence(address _address) public OnlyOwner{ 
-        allowance[_address] = true; //belli bir adrese yetki tanimladim
+        allowance[owner][_address] = true; //belli bir adrese yetki tanimladim
 
     }
+    function getSizeOfErrorMessages() public view returns(uint){
 
+        return errorMessages.length;
+    }
 
 }

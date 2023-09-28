7cf28cf65b8481f52271e9a434514c68dc5b441a
 rumeysayilma <yilmazr16@itu.edu.tr>
2023-09-28 22:25:53 +0300

    Solidity-giris-

--- contracts/SimpleContract.sol
+++ contracts/SimpleContract.sol
@@ -4,16 +4,32 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract SimpleContract {
     bool public allowed;
+    uint public count;
+    int public signedCount;
+    string private errorMessage = unicode"bu özellik aktif değil";
+
     function setAllowed(bool _allowed) public {
         allowed = _allowed;
     }
     modifier isAllowed() {
-        require(allowed,"is not allowed");
+        require(allowed,errorMessage);
         _;
     }
 
-    function buy() public isAllowed {
+    function buy() isAllowed public  {
 
+    }
+
+    function oneIncrement() public {
+        ++count;
+    }
+
+    function increment(uint _increment) isAllowed public{
+        count = count + _increment;
+    }
+
+    function signedIncrement(int _increment) public {
+        signedCount = signedCount + _increment;
     }
 
 
